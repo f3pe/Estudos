cc59bc2d281a6f49bd5d43f6cec83a2ac9f8f1a6
Felipe Coutinho <76482505+f3pe@users.noreply.github.com>
2023-01-02 08:09:30 -0300

    Add files via upload

---
+++ python/exercicios/ex_curso-em-video/ex097.py
@@ -0,0 +1,12 @@
+def escreva(palavra):
+    print("~"*len(palavra))
+    print(palavra)
+    print("~"*len(palavra))
+
+def main():
+    escreva("Felipe")
+    escreva("Curso em video")
+    escreva("any")
+
+if __name__=="__main__":
+    main()
