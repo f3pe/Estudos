63b27219c76324241305fe72669f9bc41c568056
Felipe Coutinho <76482505+f3pe@users.noreply.github.com>
2023-03-02 18:48:02 -0300

    Update Linked_list.py

--- python/Estrutura de dados/lists/Linked_list.py
+++ python/Estrutura de dados/lists/Linked_list.py
@@ -7,4 +7,17 @@ class linkedList:
     def __init__(self) -> None:
         self._current = None
         self.head = None
+
+    def __iter__(self):
+        self._current = self.head
+        return self
+    
+    def __next__(self) -> any:
+        if self._current == None:
+            raise StopIteration
+        temp = self._current
+        self._current = self._current.next
+        return temp.item
+    
+
     
